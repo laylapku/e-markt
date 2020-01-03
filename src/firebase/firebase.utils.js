2424ce0a69b13eb6c8505f6af372f4c66bbdb729
laylapku <ouyangleilei515@gmail.com>
2020-01-04 00:17:30 +0800

    migrated local shop data to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,8 +17,8 @@ firebase.initializeApp(config);
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`); // firebse automatically generates uid for new user -> always able to get ref
-  const snapShot = await userRef.get(); // get snapshot of existing user in firestore
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // firebse automatically generates uid for new user -> always able to get query ref
+  const snapShot = await userRef.get(); // get snapshot of existing user in firestore through ref
   // add new user info to firestore
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -35,6 +35,40 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     }
   }
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  // batch all calls together to prevent interruption due to network unstability
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
